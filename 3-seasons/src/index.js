9ab69c251ec5bec0dbf953e54db169b80580a459
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-16 14:57:55 -0700

    refactor for compDidMount

--- 3-seasons/src/index.js
+++ 3-seasons/src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  // constructor is used when we want to create some initial setup when our component is created. It is optional.
   constructor(props) {
-    //must call super()
     super(props);
+    this.state = { lat: null, errorMessage: '' };
+  }
 
-    // this is the only place we do direct assignment
-    // to this.state
-    this.state = { lat: '', errorMessage: '' };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log(position);
-        // setState() to change state
         this.setState({ lat: position.coords.latitude });
       },
       (err) => {
@@ -22,6 +18,10 @@ class App extends React.Component {
         this.setState({ lat: '', errorMessage: err.message });
       }
     );
+  }
+
+  componentDidUpdate() {
+    console.log('Update occured');
   }
 
   render() {
