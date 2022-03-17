eda9a791609456b20fbaa32474a323d6c7f5f6bd
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-16 17:09:40 -0700

    add css

--- 3-seasons/src/SeasonDisplay.js
+++ 3-seasons/src/SeasonDisplay.js
@@ -1,7 +1,43 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>SeasonDisplay</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: 'snowflake',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    // must be in northern hemisphere
+    // if true, then return summer. False returns winter. Ternary function.
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    //must be in south hemisphere
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
+
+//new Date().getMonth() returns a number between 0 and 11.
+// 0 starts at Jan, 11 means Dec.
+// will say summer in Northern hemisphere is between 3-8
--- 3-seasons/src/index.js
+++ 3-seasons/src/index.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: '' };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
         this.setState({ lat: position.coords.latitude });
       },
       (err) => {
-        console.log(err);
         this.setState({ lat: '', errorMessage: err.message });
       }
     );
-  }
-
-  componentDidUpdate() {
-    console.log('Update occured');
   }
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
-    if (!this.state.errorMessage & this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    } else {
-      return <div>Loading 🙃</div>;
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
     }
+
+    return (
+      <div>
+        <i className="spinner loading icon"></i>
+      </div>
+    );
   }
 }
 
