1103d9c808e86d80801ce110510c01dae04e643f
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-16 17:44:08 -0700

    default props

--- 3-seasons/src/Loader.js
+++ 3-seasons/src/Loader.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Loader extends Component {
-  render() {
-    return (
-      <div class="ui segment season-display">
-        <div class="ui active dimmer">
-          <div class="ui massive text loader">Loading</div>
-        </div>
-      </div>
-    );
-  }
-}
+const Loader = (props) => {
+  return (
+    <div class="ui active dimmer">
+      <div class="ui massive text loader">{props.message}</div>
+    </div>
+  );
+};
+
+Loader.defaultProps = { message: 'Loading...' };
+
+export default Loader;
