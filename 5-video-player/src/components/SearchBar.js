f555339de5301be7d3a82c5d49fc3a592a792d3c
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-19 08:05:10 -0700

    SearchBar

--- 5-video-player/src/components/SearchBar.js
+++ 5-video-player/src/components/SearchBar.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  state = { term: '', placeholder: 'Search' };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: make sure we call callback from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <div class="ui category search">
+          <div onSubmit={this.onFormSubmit} class="ui icon input">
+            <input
+              class="prompt"
+              type="text"
+              placeholder={this.state.placeholder}
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+            <i class="search icon"></i>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
