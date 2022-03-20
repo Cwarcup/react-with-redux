07bad9a39640d896b61771cbf6511e984181248c
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-20 10:33:48 -0700

    VideoItem title desc css

--- 5-video-player/src/components/VideoItem.js
+++ 5-video-player/src/components/VideoItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      {video.snippet.title}
+    <div className="video-item item">
       <img
+        className="ui image"
         src={video.snippet.thumbnails.medium.url}
         alt={video.snippet.chanelTitle}
       />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+        <div class="description">{video.snippet.description}</div>
+      </div>
     </div>
   );
 };
