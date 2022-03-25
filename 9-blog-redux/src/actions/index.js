6bfa0195539dfdad324d8b5554c6719bcc2605e0
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-25 15:01:59 -0700

    thunk

--- 9-blog-redux/src/actions/index.js
+++ 9-blog-redux/src/actions/index.js
@@ -1,1 +1,7 @@
-// rxaction snippet
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response });
+};
--- 9-blog-redux/src/reducers/index.js
+++ 9-blog-redux/src/reducers/index.js
@@ -1,1 +1,7 @@
 // rxreducer snippet
+
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  replaceMe: () => 'replaceMe',
+});
