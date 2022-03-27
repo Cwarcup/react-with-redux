59f2adf25e0f30b5a2946fd28c4c7c47ca45c454
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-27 10:10:13 -0700

    using lodash chain()

--- 9-blog-redux/src/actions/index.js
+++ 9-blog-redux/src/actions/index.js
@@ -8,9 +8,12 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 //   - iterate over unique `usrId`s and call `fetchUser` for each userId
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-  await dispatch(fetchPosts()); // call `fetchPosts`, need await to wait for `fetchPosts` to finish
-  const userIds = _.uniq(_.map(getState().posts, 'userId')); // get list of unique userIds from posts using lodash
-  userIds.forEach((id) => dispatch(fetchUser(id))); // iterate over userIds and call `fetchUser` for each userId
+  await dispatch(fetchPosts()); //call `fetchPosts`, need await to wait for `fetchPosts` to finish
+  _.chain(getState().posts) // get the posts from the state
+    .map('userId') // get the userIds from the posts
+    .uniq() // get list of unique userIds from posts using lodash
+    .forEach((id) => dispatch(fetchUser(id))) // iterate over userIds and call `fetchUser` for each userId
+    .value(); // execute the chain method
 };
 
 // fetch multiple posts from jsonPlaceholder
