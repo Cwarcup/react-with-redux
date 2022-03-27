b97063c9567e24dd83eb139d00c61411bceb6159
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-27 09:57:21 -0700

    solution for over fetching issue

--- 9-blog-redux/src/actions/index.js
+++ 9-blog-redux/src/actions/index.js
@@ -7,10 +7,10 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 //   - find all unique `userId`s from the posts
 //   - iterate over unique `usrId`s and call `fetchUser` for each userId
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log('About to fetchPostsAndUsers');
-  await dispatch(fetchPosts());
-  console.log('fetchPostsAndUsers');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts()); // call `fetchPosts`, need await to wait for `fetchPosts` to finish
+  const userIds = _.uniq(_.map(getState().posts, 'userId')); // get list of unique userIds from posts using lodash
+  userIds.forEach((id) => dispatch(fetchUser(id))); // iterate over userIds and call `fetchUser` for each userId
 };
 
 // fetch multiple posts from jsonPlaceholder
