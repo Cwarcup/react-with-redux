f9e462e61e40387b3aa30aab24e2409afc9bd0c5
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-27 09:43:47 -0700

    fetchPostsAndUsers

--- 9-blog-redux/src/actions/index.js
+++ 9-blog-redux/src/actions/index.js
@@ -1,6 +1,18 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+// fetchPostsAndUsers() will...
+//   - call `fetchPosts`
+//   - get a list of posts
+//   - find all unique `userId`s from the posts
+//   - iterate over unique `usrId`s and call `fetchUser` for each userId
+
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  console.log('About to fetchPostsAndUsers');
+  await dispatch(fetchPosts());
+  console.log('fetchPostsAndUsers');
+};
+
 // fetch multiple posts from jsonPlaceholder
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
@@ -9,12 +21,9 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 // fetch one individual user at a time from jsonPlaceholder
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
