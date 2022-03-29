a673a715e7bedd91573c11c06d3d98b55c3b3e06
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-29 16:20:42 -0700

    render error

--- 10-streams/client/src/components/streams/StreamCreate.js
+++ 10-streams/client/src/components/streams/StreamCreate.js
@@ -4,17 +4,27 @@ import { Field, reduxForm } from 'redux-form';
 class StreamCreate extends Component {
   // creates a field for the form
   // passed to the form as a prop
-  renderInput({ input, label, meta }) {
+  renderInput = ({ input, label, meta, touched }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`; // if true then add error class
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
+  };
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
   }
 
-  //
   onSubmit(formValues) {
     console.log(formValues);
   }
@@ -23,7 +33,7 @@ class StreamCreate extends Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field
           name="title"
