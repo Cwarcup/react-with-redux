886b4c014db043f4da278ea7088212a2f29efb56
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-29 08:38:19 -0700

    refactor for actions

---
+++ 10-streams/client/src/actions/types.js
@@ -0,0 +1,5 @@
+// can create variables to represent the different types of actions.
+// no longer using strings in our actual actions to represent the type of action.
+
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
