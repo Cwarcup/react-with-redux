fa542d1493d8bdd26b4240eaed9131dde346d0b7
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-29 15:18:02 -0700

    validatoing form inputs

--- 10-streams/client/src/components/streams/StreamCreate.js
+++ 10-streams/client/src/components/streams/StreamCreate.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  // creates a field for the form
+  // passed to the form as a prop
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
 
+  //
   onSubmit(formValues) {
     console.log(formValues);
   }
@@ -37,6 +41,24 @@ class StreamCreate extends Component {
   }
 }
 
+// validate function is used to validate the form
+// formValues will contain all the values of the form (title and description)
+const validate = (formValues) => {
+  const errors = {};
+
+  // no title
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  // no description
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'streamCreateForm', // a unique name for the form
+  validate: validate, // validate function
 })(StreamCreate);
