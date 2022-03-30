d4b0a649a3033fff31888d2243981b17d957681a
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-30 14:24:21 -0700

    associate stream with user

--- 10-streams/client/src/actions/index.js
+++ 10-streams/client/src/actions/index.js
@@ -24,32 +24,33 @@ export const signOut = () => {
 };
 
 // attempt to make request to API server to create a new stream
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues);     // second argument is the data we want to send to the server
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth; // getState() returns the entire state of the application, including the auth object.
+  const response = await streams.post('/streams', { ...formValues, userId }); // second argument is the data we want to send to the server
 
-  dispatch({ type: CREATE_STREAM, payload: response.data });       // payload contains the data we want inside of response
+  dispatch({ type: CREATE_STREAM, payload: response.data }); // payload contains the data we want inside of response
 };
 
 export const fetchStreams = () => async (dispatch) => {
   const response = await streams.get('/streams');
 
   dispatch({ type: FETCH_STREAMS, payload: response.data });
-}
+};
 
 export const fetchStream = (id) => async (dispatch) => {
   const response = await streams.get(`/streams/${id}`);
 
   dispatch({ type: FETCH_STREAM, payload: response.data });
-}
+};
 
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
-}
+};
 
 export const editStream = (id, formValues) => async (dispatch) => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
-}
+};
--- 10-streams/client/src/components/streams/StreamList.js
+++ 10-streams/client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import streams from '../../apis/streams';
 
 // display a list of streams
 // will use a class based component because we want to use componentDidMount() to fetch the data one time.
@@ -10,9 +11,26 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
-  
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
