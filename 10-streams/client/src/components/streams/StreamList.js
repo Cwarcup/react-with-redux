cb983cafa7e859723a72891cb94367f1e6b170cb
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-30 14:45:50 -0700

    add renderAdmin method in StreamList

--- 10-streams/client/src/components/streams/StreamList.js
+++ 10-streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
-import streams from '../../apis/streams';
 
 // display a list of streams
 // will use a class based component because we want to use componentDidMount() to fetch the data one time.
@@ -11,10 +10,23 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  // method to render edit and delete buttons of logged in user created the stream
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
             {stream.title}
@@ -24,6 +36,7 @@ class StreamList extends Component {
       );
     });
   }
+  
   render() {
     return (
       <div>
@@ -35,7 +48,10 @@ class StreamList extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
   // remember, our streams are an object. Need to get the values out of it.
   // use Object.values() to get the values out of the object.
   // now we have a prop called streams that is an array of all our streams.
