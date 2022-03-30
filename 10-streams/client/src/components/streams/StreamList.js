24fe993928577ee15da6d5df566dd8417f65e1bb
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-30 13:50:20 -0700

    streamList component

--- 10-streams/client/src/components/streams/StreamList.js
+++ 10-streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+// display a list of streams
+// will use a class based component because we want to use componentDidMount() to fetch the data one time.
 
-export default StreamList;
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    return <div>StreamList</div>;
+  }
+}
+
+export default connect(null, { fetchStreams })(StreamList);
