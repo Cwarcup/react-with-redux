6ec458680786d99a2ab413d752c8372b2e198162
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-30 13:39:18 -0700

    add streamReducer and FETCH_STREAMS

--- 10-streams/client/src/reducers/index.js
+++ 10-streams/client/src/reducers/index.js
@@ -2,21 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { EDIT_STREAM } from '../actions/types';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
-
-const streamReducer = (state={}, action) => {
-  switch (action.type) {
-    case EDIT_STREAM:
-      // const newState = { ...state };           // old way
-      // newState[action.payload.id] = action.payload;
-      // return newState;
-
-      return { ...state, [action.payload.id]: action.payload }; // ES6 way, this is known as key interpolation
-    default:
-      return state;
-  }
-}
--- 10-streams/client/src/reducers/streamReducer.js
+++ 10-streams/client/src/reducers/streamReducer.js
@@ -5,26 +5,27 @@ import {
   FETCH_STREAMS,
   CREATE_STREAM,
   EDIT_STREAM,
-  DELETE_STREAM
-} from '../actions/types'
-
+  DELETE_STREAM,
+} from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') }; // use lodash to map the payload into a new object with the id as the key
 
-  case FETCH_STREAM:
-    return { ...state, [action.payload.id]: action.payload };  //ES6 way of adding a new key to an object
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload }; //ES6 way of adding a new key to an object
 
-  case CREATE_STREAM:
-    return {...state, [action.payload.id]: action.payload}
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
 
-  case EDIT_STREAM:
-    return {...state, [action.payload.id]: action.payload}
-  
-  case DELETE_STREAM:
-    return _.omit(state, action.payload);
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
 
-  default:
-    return state
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+
+    default:
+      return state;
   }
-}
+};
