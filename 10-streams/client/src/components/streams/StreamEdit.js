ba2481822762c652699f172b6bef3a607d37f112
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-31 09:15:41 -0700

    connect for StreamEdit

--- 10-streams/client/src/components/streams/StreamEdit.js
+++ 10-streams/client/src/components/streams/StreamEdit.js
@@ -1,11 +1,14 @@
 // need to communicate which which stream is being edited
 
 import React from 'react';
+import { connect } from 'react-redux';
 
 const StreamEdit = (props) => {
-  console.log(props);
+  console.log('props', props);
   return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
-
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }; // props will now contain a stream property, with the stream we want to edit.
+};
+export default connect(mapStateToProps)(StreamEdit);
