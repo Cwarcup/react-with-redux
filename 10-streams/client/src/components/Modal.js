958d283ac3cb6f6c8286f2f69aa5b03ae5f625bb
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-31 13:47:41 -0700

    modal props

--- 10-streams/client/src/components/Modal.js
+++ 10-streams/client/src/components/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
+
 
 // create a portal to render the modal in
 // need these divs to be rendered directly from the body element.
@@ -8,21 +8,16 @@ import history from '../history';
 const Modal = (props) => {
   return ReactDOM.createPortal(
     <div
-      onClick={() => history.push('/')}
+      onClick={props.onDismiss}
       className="ui dimmer modals visible active"
     >
       <div
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui cancel button">Cancel</button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
