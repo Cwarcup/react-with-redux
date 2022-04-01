38a325fd8da44e19994e248b3564514786dff391
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-01 11:03:48 -0700

    update with react final form

--- 10-streams/client/src/components/streams/StreamForm.js
+++ 10-streams/client/src/components/streams/StreamForm.js
@@ -5,24 +5,11 @@
 
 // instead of calling an action creator when its created, it should call some callback that gets passed from the parent component
 
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React from 'react';
+import { Field, Form } from 'react-final-form';
 
-class StreamForm extends Component {
-  // creates a field for the form
-  // passed to the form as a prop
-  renderInput = ({ input, label, meta, touched }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`; // if true then add error class
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
-  renderError({ error, touched }) {
+const StreamForm = (props) => {
+  const renderError = ({ error, touched }) => {
     if (touched && error) {
       return (
         <div className="ui error message">
@@ -30,55 +17,57 @@ class StreamForm extends Component {
         </div>
       );
     }
-  }
+  };
+  // creates a field for the form
+  // passed to the form as a prop
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`; // if true then add error class
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {renderError(meta)}
+      </div>
+    );
+  };
 
   // if form passes validation, we call onSubmit, which calls create stream action
   // want to call a callback function that gets passed from the parent component
-  onSubmit = (formValues) => {
-    this.props.onSubmit(formValues);
+  const onSubmit = (formValues) => {
+    props.onSubmit(formValues);
     // expect the parent to have a callback function called onSubmit
   };
 
-  render() {
-    return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field
-          name="title"
-          component={this.renderInput}
-          label="Enter Tittle "
-        />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description "
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
-    );
-  }
-}
+  return (
+    <Form
+      initialValues={props.initialValues}
+      onSubmit={onSubmit}
+      validate={(formValues) => {
+        const errors = {};
 
-// validate function is used to validate the form
-// formValues will contain all the values of the form (title and description)
-const validate = (formValues) => {
-  const errors = {};
+        if (!formValues.title) {
+          errors.title = 'You must enter a title';
+        }
 
-  // no title
-  if (!formValues.title) {
-    errors.title = 'You must enter a title';
-  }
-  // no description
-  if (!formValues.description) {
-    errors.description = 'You must enter a description';
-  }
+        if (!formValues.description) {
+          errors.description = 'You must enter a description';
+        }
 
-  return errors;
+        return errors;
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} className="ui form error">
+          <Field name="title" component={renderInput} label="Enter Title" />
+          <Field
+            name="description"
+            component={renderInput}
+            label="Enter Description"
+          />
+          <button className="ui button primary">Submit</button>
+        </form>
+      )}
+    />
+  );
 };
 
-export default reduxForm({
-  form: 'streamForm', // a unique name for the form
-  validate: validate, // validate function
-})(StreamForm);
+export default StreamForm;
