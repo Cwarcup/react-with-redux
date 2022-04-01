6323f555647d5f891b86164083975ab3e4c3c584
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-01 11:59:36 -0700

    onLanguageChange

--- 11-translate-context/src/components/App.js
+++ 11-translate-context/src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag ca"
+            onClick={() => this.onLanguageChange('english')}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          ></i>
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
